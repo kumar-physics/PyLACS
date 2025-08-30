27b941c0858c3198de6a65ddd51a5254dfbe2dc1
Kumaran <kumar.physics@gmail.com>
2025-08-29 23:18:06 -0400

    results added

---
+++ scripts/read_json.py
@@ -0,0 +1,35 @@
+import csv
+import json
+
+def read_csv_file(csvfile):
+    fo=open('results.csv','w')
+    with open(csvfile,'r') as csvFile:
+        csvF = csv.reader(csvFile)
+        for row in csvF:
+            bmrb_id = row[0]
+            print (bmrb_id)
+            l=f'{bmrb_id},{row[4]},{row[6]},{row[8]},{row[5]},{row[7]},{row[9]}'
+            if bmrb_id not in  ['4077','4675','5168']:
+                for method in ["tukey", "theilsen", "ransac", "quantile", "bayes"]:
+                    with open(f'../scratch/{bmrb_id}/{bmrb_id}_{method}.json','r') as f:
+                        data = json.load(f)
+                        of = data['1']['offsets']
+                        try:
+                            ca = -of['ca']
+                        except KeyError:
+                            cs = ''
+                        try:
+                            c = -of['c']
+                        except KeyError:
+                            c=''
+                        try:
+                            h= -of['h']
+                        except KeyError:
+                            h=''
+                        l = f'{l},{ca},{c},{h}'
+            fo.write(f'{l}\n')
+
+
+
+if __name__ == "__main__":
+    read_csv_file('Table_S1_clean.csv')
--- scripts/run_lacs.py
+++ scripts/run_lacs.py
@@ -6,19 +6,23 @@ from typing import Dict, List, Optional, Sequence, Tuple
 
 
 def read_csv_file(csvfile):
+    flg=False
     with open(csvfile,'r') as csvFile:
         csvF = csv.reader(csvFile)
-        for rows in csvF:
+        for row in csvF:
             bmrb_id = row[0]
-            print (rows)
-            str_file = f'/reboxitory/2025/08/BMRB/macromolecules/bmr{bmrb_id}/bmr{bmrb_id}_3.str'
-            data_id = bmrb_id
-            for method in ["tukey","theilsen","ransac","quantile","bayes"]:
-                out_dir = Path(f'../scratch/{data_id}')
-                plots = True
-                report=run_lacs(str_file=str_file,method=method,data_id=data_id,outdir=out_dir,plots=plots)
-                with open(out_dir /f'{data_id}_{method}.json', "w", encoding="utf-8") as f:
-                    json.dump(report, f, indent=2)
+            print (bmrb_id)
+            if bmrb_id == '5168':
+                flg = True
+            if flg and bmrb_id not in ['4077','24152','5168']:
+                str_file = f'/reboxitory/2025/08/BMRB/macromolecules/bmr{bmrb_id}/bmr{bmrb_id}_3.str'
+                data_id = bmrb_id
+                for method in ["tukey","theilsen","ransac","quantile","bayes"]:
+                    out_dir = Path(f'../scratch/{data_id}')
+                    plots = True
+                    report=run_lacs(str_file=str_file,method=method,data_id=data_id,outdir=out_dir,plots=plots)
+                    with open(out_dir /f'{data_id}_{method}.json', "w", encoding="utf-8") as f:
+                        json.dump(report, f, indent=2)
 
 
 if __name__ == "__main__":
