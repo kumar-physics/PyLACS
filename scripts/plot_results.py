c9ee3761f415a4e991c888fd15cc47905f1bb2b3
Kumaran Baskaran <baskaran@uchc.edu>
2025-08-31 07:07:12 -0400

    plot scripts updated

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -6,9 +6,10 @@ def plot_csv(csvfile):
     with open(csvfile,'r') as csvFile:
         csvf = csv.reader(csvFile)
         data = list(csvf)
-    clean_data = [x for x in data if len(x)==22]
+    clean_data = [x for x in data if len(x)==31]
     for x in data:
-        if len(x)!=22:
+        print (len(x))
+        if len(x)!=31:
             print (x)
     result = [list(x) for x in zip(*clean_data)]
     print (result)
@@ -16,25 +17,28 @@ def plot_csv(csvfile):
         'BMRB_ID':result[0],
         'RefDB(CA)':[float(i) if i!='' else None for i in result[1]],
         'RefDB(C)':[float(i) if i!='' else None for i in result[2]],
-        'RefDB(H)':[float(i) if i!='' else None for i in result[3]],
+        'RefDB(HA)':[float(i) if i!='' else None for i in result[3]],
         'LACS(CA)': [float(i) if i != '' else None for i in result[4]],
         'LACS(C)': [float(i) if i != '' else None for i in result[5]],
-        'LACS(H)': [float(i) if i != '' else None for i in result[6]],
+        'LACS(HA)': [float(i) if i != '' else None for i in result[6]],
         'Tukey(CA)': [float(i) if i!='' else None for i in result[7]],
         'Tukey(C)': [float(i) if i!='' else None for i in result[8]],
-        'Tukey(H)': [float(i) if i!='' else None for i in result[9]],
+        'Tukey(HA)': [float(i) if i!='' else None for i in result[9]],
         'Theilsen(CA)': [float(i) if i != '' else None for i in result[10]],
         'Theilsen(C)': [float(i) if i != '' else None for i in result[11]],
-        'Theilsen(H)': [float(i) if i != '' else None for i in result[12]],
+        'Theilsen(HA)': [float(i) if i != '' else None for i in result[12]],
         'Ransac(CA)': [float(i) if i != '' else None for i in result[13]],
         'Ransac(C)': [float(i) if i != '' else None for i in result[14]],
-        'Ransac(H)': [float(i) if i != '' else None for i in result[15]],
+        'Ransac(HA)': [float(i) if i != '' else None for i in result[15]],
         'Quantile(CA)': [float(i) if i != '' else None for i in result[16]],
         'Quantile(C)': [float(i) if i != '' else None for i in result[17]],
-        'Quantile(H)': [float(i) if i != '' else None for i in result[18]],
+        'Quantile(HA)': [float(i) if i != '' else None for i in result[18]],
         'Bayes(CA)': [float(i) if i != '' else None for i in result[19]],
         'Bayes(C)': [float(i) if i != '' else None for i in result[20]],
-        'Bayes(H)': [float(i) if i != '' else None for i in result[21]],
+        'Bayes(HA)': [float(i) if i != '' else None for i in result[21]],
+        'Bayes(CA_SD)': [float(i) if i != '' else None for i in result[24]],
+        'Bayes(C_SD)': [float(i) if i != '' else None for i in result[27]],
+        'Bayes(HA_SD)': [float(i) if i != '' else None for i in result[30]],
 
 
 
@@ -52,9 +56,9 @@ def plot_csv(csvfile):
             dash="dash"  # Optional: 'solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot'
         )
     )
-    fig.show()
-    fig = px.scatter(df, x='RefDB(H)',
-                     y=['LACS(H)', 'Tukey(H)', 'Theilsen(H)', 'Ransac(H)', 'Quantile(H)', 'Bayes(H)'],
+    fig.write_html('RefDBvsLACS_CA_Scatter.html')
+    fig = px.scatter(df, x='RefDB(HA)',
+                     y=['LACS(HA)', 'Tukey(HA)', 'Theilsen(HA)', 'Ransac(HA)', 'Quantile(HA)', 'Bayes(HA)'],
                      hover_name='BMRB_ID')
     fig.add_shape(
         type="line",
@@ -68,7 +72,7 @@ def plot_csv(csvfile):
             dash="dash"  # Optional: 'solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot'
         )
     )
-    fig.show()
+    fig.write_html('RefDBvsLACS_HA_Scatter.html')
     fig = px.scatter(df, x='RefDB(C)',
                      y=['LACS(C)', 'Tukey(C)', 'Theilsen(C)', 'Ransac(C)', 'Quantile(C)', 'Bayes(C)'],
                      hover_name='BMRB_ID')
@@ -84,8 +88,49 @@ def plot_csv(csvfile):
             dash="dash"  # Optional: 'solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot'
         )
     )
-    fig.show()
-    fig = px.histogram(df,x=[df['RefDB(C)']-df['LACS(C)'],df['RefDB(C)']-df['Bayes(C)']],barmode='overlay')
-    fig.show()
+    fig.write_html('RefDBvsLACS_C_Scatter.html')
+    fig = px.scatter(df, x='RefDB(CA)', y = 'Bayes(CA)', error_y='Bayes(CA_SD)',hover_name='BMRB_ID')
+    fig.add_shape(
+        type="line",
+        x0=-2,  # Starting x-coordinate of the line
+        y0=-2,  # Starting y-coordinate of the line
+        x1=3,  # Ending x-coordinate of the line
+        y1=3,  # Ending y-coordinate of the line
+        line=dict(
+            color="red",
+            width=2,
+            dash="dash"  # Optional: 'solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot'
+        )
+    )
+    fig.write_html('RefDBvsLACS_Bayes_CA_Scatter.html')
+    fig = px.scatter(df, x='RefDB(C)', y='Bayes(C)', error_y='Bayes(C_SD)', hover_name='BMRB_ID')
+    fig.add_shape(
+        type="line",
+        x0=-3,  # Starting x-coordinate of the line
+        y0=-3,  # Starting y-coordinate of the line
+        x1=4,  # Ending x-coordinate of the line
+        y1=4,  # Ending y-coordinate of the line
+        line=dict(
+            color="red",
+            width=2,
+            dash="dash"  # Optional: 'solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot'
+        )
+    )
+    fig.write_html('RefDBvsLACS_Bayes_C_Scatter.html')
+    fig = px.scatter(df, x='RefDB(HA)', y='Bayes(HA)', error_y='Bayes(HA_SD)', hover_name='BMRB_ID')
+    fig.add_shape(
+        type="line",
+        x0=-0.5,  # Starting x-coordinate of the line
+        y0=-0.5,  # Starting y-coordinate of the line
+        x1=0.4,  # Ending x-coordinate of the line
+        y1=0.4,  # Ending y-coordinate of the line
+        line=dict(
+            color="red",
+            width=2,
+            dash="dash"  # Optional: 'solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot'
+        )
+    )
+    fig.write_html('RefDBvsLACS_Bayes_HA_Scatter.html')
+
 if __name__ == "__main__":
-    plot_csv('results.csv')
+    plot_csv('results2.csv')
--- scripts/read_json.py
+++ scripts/read_json.py
@@ -2,7 +2,7 @@ import csv
 import json
 
 def read_csv_file(csvfile):
-    fo=open('results.csv','w')
+    fo=open('results2.csv','w')
     with open(csvfile,'r') as csvFile:
         csvF = csv.reader(csvFile)
         for row in csvF:
@@ -11,7 +11,7 @@ def read_csv_file(csvfile):
             l=f'{bmrb_id},{row[4]},{row[6]},{row[8]},{row[5]},{row[7]},{row[9]}'
             if bmrb_id not in  ['4077','5168']:
                 for method in ["tukey", "theilsen", "ransac", "quantile", "bayes"]:
-                    with open(f'../scratch/{bmrb_id}/{bmrb_id}_{method}.json','r') as f:
+                    with open(f'../scratch/scratch/{bmrb_id}/{bmrb_id}_{method}.json','r') as f:
                         data = json.load(f)
                         of = data['1']['offsets']
                         try:
@@ -27,6 +27,35 @@ def read_csv_file(csvfile):
                         except KeyError:
                             h=''
                         l = f'{l},{ca},{c},{h}'
+                        if method == 'bayes':
+                            ofb = data['1']['offsets_bayes']
+                            try:
+                                ca_min = ofb['ca']['ci95'][0]
+                                ca_max = ofb['ca']['ci95'][1]
+                                ca_sd = ofb['ca']['sd']
+                            except KeyError:
+                                ca_min=''
+                                ca_max = ''
+                                ca_sd = ''
+                            try:
+                                c_min = ofb['c']['ci95'][0]
+                                c_max = ofb['c']['ci95'][1]
+                                c_sd = ofb['c']['sd']
+                            except KeyError:
+                                c_min=''
+                                c_max = ''
+                                c_sd = ''
+                            try:
+                                ha_min = ofb['ha']['ci95'][0]
+                                ha_max = ofb['ha']['ci95'][1]
+                                ha_sd = ofb['ha']['sd']
+                            except KeyError:
+                                ha_min=''
+                                ha_max = ''
+                                ha_sd = ''
+                            l = f'{l},{ca_min},{ca_max},{ca_sd},{c_min},{c_max},{c_sd},{ha_min},{ha_max},{ha_sd}'
+
+
                 fo.write(f'{l}\n')
 
 
